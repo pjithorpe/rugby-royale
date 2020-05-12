1a608c6a2ea66a753217044025cd8d8bb1772558
pjithorpe <pjithorpe@gmail.com>
2020-05-12 18:12:25 +0100

    Add player stats object.

---
+++ Entities/GameObjects/PlayerStats/PlayerStats.cs
@@ -0,0 +1,12 @@
+﻿namespace RugbyRoyale.Entities.GameObjects
+{
+    public class PlayerStats
+    {
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public int Physicality { get; set; }
+        public int Stamina { get; set; }
+        public int Handling { get; set; }
+        public int Kicking { get; set; }
+    }
+}
