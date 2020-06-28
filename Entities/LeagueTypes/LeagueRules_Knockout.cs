c97c12ed23f9f73ac8740db6648d1b43507a8b24
Patrick Thorpe <pjithorpe@gmail.com>
2020-06-28 14:25:56 +0100

    Add description for knockout league rules.

--- Entities/LeagueTypes/LeagueRules_Knockout.cs
+++ Entities/LeagueTypes/LeagueRules_Knockout.cs
@@ -8,7 +8,7 @@ namespace RugbyRoyale.Entities.LeagueTypes
     public class LeagueRules_Knockout : LeagueRules
     {
         public override string Name => "Straight Knockout";
-        public override string Description => "TODO";
+        public override string Description => "A single elimination tournament in which teams are paired head-to-head in a series of rounds.";
         public override LeagueType LeagueType => LeagueType.Knockout;
 
         public override int MinSize => 8;
