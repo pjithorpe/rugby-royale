a5b6bd8108c540961083b3cd7353c5bdb32b0b12
Patrick Thorpe <pjithorpe@gmail.com>
2020-07-21 01:40:43 +0100

    Create new player focus calculation algorithm.

--- Entities/Extensions/Model/PlayerExtensions.cs
+++ Entities/Extensions/Model/PlayerExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using RugbyRoyale.Entities.Enums;
 using RugbyRoyale.Entities.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -49,15 +50,73 @@ namespace RugbyRoyale.Entities.Extensions
             }
 
             // Nuanced difference
-            // TODO: Too naive, needs work
-            double attPoints = player.Attack + player.Handling * 0.75 + player.Kicking * 0.5;
-            double defPoints = player.Defence + player.Physicality * 0.75 + player.Stamina * 0.5;
+            var allStats = new int[] { player.Attack, player.Defence, player.Physicality, player.Stamina, player.Handling, player.Kicking };
 
-            if (attPoints >= defPoints + 20)
+            // Normalised
+            int weakestSkill = allStats.Min();
+            int normalisedAtt = player.Attack - weakestSkill;
+            int normalisedDef = player.Defence - weakestSkill;
+            int normalisedPhy = player.Physicality - weakestSkill;
+            int normalisedSta = player.Stamina - weakestSkill;
+            int normalisedHan = player.Handling - weakestSkill;
+            int normalisedKic = player.Kicking - weakestSkill;
+
+            // Mean deviations
+            double meanSkill = allStats.Average();
+            double meanDevAtt = player.Attack - meanSkill;
+            double meanDevDef = player.Defence - meanSkill;
+            double meanDevPhy = player.Physicality - meanSkill;
+            double meanDevSta = player.Stamina - meanSkill;
+            double meanDevHan = player.Handling - meanSkill;
+            double meanDevKic = player.Kicking - meanSkill;
+
+            double attackingScore = meanDevAtt;
+            double defendingScore = meanDevDef;
+
+            // Physicality adds to good attack or defence
+            if ((meanDevDef < -5 && meanDevAtt > 5) || (meanDevAtt > meanSkill && player.Attack >= player.Defence + 15))
             {
-                return PlayerFocus.Attacking;
+                // If physicality is good, it assists attack
+                if (meanDevPhy > 0 || player.Physicality > player.Attack)
+                {
+                    attackingScore += meanDevPhy;
+                }
             }
-            else if (defPoints >= attPoints + 20)
+            else if ((meanDevAtt < -5 && meanDevDef > 5) || (meanDevDef > meanSkill && player.Defence >= player.Attack + 15))
+            {
+                // If physicality is good, it assists defence
+                if (meanDevPhy > 0 || player.Physicality > player.Defence)
+                {
+                    defendingScore += meanDevPhy;
+                }
+            }
+
+            // Kicking adds to good attack or defence
+            if ((meanDevDef < -5 && meanDevAtt > 5) || (meanDevAtt > meanSkill && player.Attack >= player.Defence + 15))
+            {
+                // If physicality is good, it assists attack
+                if (meanDevKic > 0 || player.Kicking > player.Attack)
+                {
+                    attackingScore += meanDevKic;
+                }
+            }
+            else if ((meanDevAtt < -5 && meanDevDef > 5) || (meanDevDef > meanSkill && player.Defence >= player.Attack + 15))
+            {
+                // If physicality is good, it assists defence
+                if (meanDevKic > 0 || player.Kicking > player.Defence)
+                {
+                    defendingScore += meanDevKic;
+                }
+            }
+
+            defendingScore += meanDevSta;
+            attackingScore += meanDevHan;
+
+            if (defendingScore > attackingScore * 1.25)
+            {
+                return PlayerFocus.Defending;
+            }
+            else if (attackingScore > defendingScore * 1.25)
             {
                 return PlayerFocus.Defending;
             }
