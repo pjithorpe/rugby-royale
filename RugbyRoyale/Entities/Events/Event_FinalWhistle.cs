cfee486807a2ae359501dc1debbcb6c233f5255d
Patrick Thorpe <pjithorpe@gmail.com>
2021-12-02 01:13:01 +0000

    Create events for the kickoff and final whistle.

---
+++ RugbyRoyale/Entities/Events/Event_FinalWhistle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RugbyRoyale.Entities.Events
+{
+    public class Event_FinalWhistle : MatchEvent
+    {
+        public override bool IsHalting => true;
+
+        public Event_FinalWhistle(Guid matchID, int second) : base(matchID, second)
+        {
+        }
+
+        public override string Name { get => "Final Whistle"; }
+    }
+}
---
+++ RugbyRoyale/Entities/Events/Event_KickOff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RugbyRoyale.Entities.Events
+{
+    public class Event_KickOff : MatchEvent
+    {
+        public Event_KickOff(Guid matchID, int second) : base(matchID, second)
+        {
+        }
+
+        public override string Name { get => "Kick Off"; }
+    }
+}
